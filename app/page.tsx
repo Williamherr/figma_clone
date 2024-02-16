64c3ae6d8171686a009eeb4544328a237f54a50f
William Herr <herrwilliam@outlook.com>
2024-02-16 17:42:01 -0500

    Update code: CSS overflow, LiveCursors message, import jsPDF, RoomProvider, package.json dependencies, dynamic import, RightSidebar settings

--- app/page.tsx
+++ app/page.tsx
@@ -1,66 +1,10 @@
-"use client";
+import dynamic from "next/dynamic";
 
-import { fabric } from "fabric";
-import { useEffect, useRef, useState } from "react";
+/**
+ * disable ssr to avoid pre-rendering issues of Next.js
+ *
+ * we're doing this because we're using a canvas element that can't be pre-rendered by Next.js on the server
+ */
+const App = dynamic(() => import("./app"), { ssr: false });
 
-import LeftSidebar from "@/components/LeftSidebar";
-import Live from "@/components/Live";
-import Navbar from "@/components/Navbar";
-import RightSidebar from "@/components/RightSidebar";
-import {
-  handleCanvasMouseDown,
-  handleResize,
-  initializeFabric,
-} from "@/lib/canvas";
-import { ActiveElement } from "@/types/type";
-
-export default function Page() {
-  const canvasRef = useRef<HTMLCanvasElement>(null);
-  const fabricRef = useRef<fabric.Canvas | null>(null);
-  const isDrawing = useRef(false);
-  const shapeRef = useRef<fabric.Object | null>(null);
-  const selectedShapeRef = useRef<string | null>("rectangle");
-
-  const [activeElement, setActiveElement] = useState<ActiveElement>({
-    name: "",
-    value: "",
-    icon: "",
-  });
-
-  const handleActiveElement = (element: ActiveElement) => {
-    setActiveElement(element);
-    selectedShapeRef.current = element?.value as string;
-  };
-
-  useEffect(() => {
-    const canvas = initializeFabric({ canvasRef, fabricRef });
-
-    canvas.on("mouse:down", (options) => {
-      handleCanvasMouseDown({
-        options,
-        canvas,
-        isDrawing,
-        shapeRef,
-        selectedShapeRef,
-      });
-
-      window.addEventListener("resize", () => {
-        handleResize({ canvas });
-      });
-    });
-  }, []);
-
-  return (
-    <main className="h-screen overflow-hidden">
-      <Navbar
-        activeElement={activeElement}
-        handleActiveElement={handleActiveElement}
-      />
-      <section className="flex h-full flex-row">
-        <LeftSidebar />
-        <Live canvasRef={canvasRef} />
-        <RightSidebar />
-      </section>
-    </main>
-  );
-}
+export default App;
